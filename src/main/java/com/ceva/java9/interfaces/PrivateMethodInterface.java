32eaac77561b5b6c126c55bb76f14e7e44f26d57
cevadev <carlos.villanueva.altuna@gmail.com>
2023-01-13 13:03:01 -0300

    Java Features from version 9 to 17

---
+++ src/main/java/com/ceva/java9/interfaces/PrivateMethodInterface.java
@@ -0,0 +1,47 @@
+package com.ceva.java9.interfaces;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
+public interface PrivateMethodInterface {
+    default void method1(){
+        System.out.println("In method 1");
+        try {
+            utilResource();
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    default  void method2(){
+        System.out.println("In method 2");
+        try {
+            utilResource();
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Los metodos privados son una caracteristica de Java 9
+     * Los metodos privados permiten reducir la redundancia y hacen del codigo comun mas seguro. Nos permiten
+     * compartir codigo comun entre metodos default o static
+     * Los metodos privados tambien pueden ser declarados como static con la ventaja que este metodo privado static
+     * puede ser llamado por default method static y default method no static
+     */
+    private void utilResource()throws IOException{
+        System.out.println("Reading common resources");
+        BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("data.txt"), "UTF-8"));
+        String line = br.readLine();
+        while (line != null){
+            System.out.println(line);
+            line = br.readLine();
+        }
+        br.close();
+    }
+}
---
+++ src/main/java/com/ceva/java9/interfaces/PrivateMethodInterfaceDemo.java
@@ -0,0 +1,9 @@
+package com.ceva.java9.interfaces;
+
+public class PrivateMethodInterfaceDemo implements PrivateMethodInterface{
+    public static void main(String[] args) {
+        PrivateMethodInterfaceDemo demo = new PrivateMethodInterfaceDemo();
+        demo.method1();
+        demo.method2();
+    }
+}
